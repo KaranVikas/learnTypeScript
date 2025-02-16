4bf711792715e43e7bf08888ac7faaee0661fb4c
KaranVikas <karanvikas15@gmail.com>
2025-02-16 11:43:20 -0500

    started working with typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import User from "./components/User.tsx"
+
+function App() {
+  return (
+    <>
+      <div>
+        <User name='alex' age={20} isStudent={true}/>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/User.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+
+type UserShape = {
+  name:string;
+  age: number;
+  isStudent: boolean
+}
+
+// interface UserShape {
+//   name: String;
+//   age: number;
+//   isStudent: boolean;
+// }
+
+const User: FC<UserShape> = ({name, age, isStudent}) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h1>{age}</h1>
+      <h1>{isStudent}</h1>
+    </div>
+  )
+}
+
+export default User
